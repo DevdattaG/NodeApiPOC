f3b474e012dc11f1ba4bd74fea06ccf42a0d85c7
DevdattaG <devdatta.godbole777@gmail.com>
2017-12-15 18:54:18 +0530

    Adding final CRUD

--- user/UserController.js
+++ user/UserController.js
@@ -32,4 +32,31 @@ router.get('/', function(req,res){
     });
 });
 
+// Get a single specific user
+router.get('/:id', function(req,res){
+    User.findById(req.params.id, function(err, user){
+        if(err)
+            return res.status(500).send("Encountered problem fetching user");
+        if(!user)
+            return res.status(404).send("No user found" + user);
+        return res.status(200).send(user);
+    });
+});
+
+router.delete('/:id', function(req,res){
+    User.findByIdAndRemove(req.params.id, function(err, user){
+        if(err)
+            return res.status(500).send("Encountered problem deleting user");    
+        return res.status(200).send("deleted " + user.name);
+    });
+});
+
+router.put('/:id', function(req,res){
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, user){
+        if(err)
+            return res.status(500).send("Encountered problem updating user");    
+        return res.status(200).send(user);
+    });
+});
+
 module.exports = router;
